7b05eae05c610df90145809a52f209518156f58b
AnaLiliaTeixeira <ana.lilia.teixeira2@gmail.com>
2023-03-05 15:06:05 +0000

    Wine created

---
+++ ProjetoSC/src/entities/Wine.java
@@ -0,0 +1,94 @@
+package entities;
+
+import java.io.File;
+import java.util.HashMap;
+
+public class Wine {
+
+	private String name;
+	private File image;
+	private int quantity;
+	private int price;
+	private HashMap<String, Byte> classifications;
+
+	public Wine(String name, File image) {
+		this.name = name;
+		this.image = image;
+		this.quantity = 0;
+		this.classifications = new HashMap<>();
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the image
+	 */
+	public File getImage() {
+		return image;
+	}
+
+	/**
+	 * @param image the image to set
+	 */
+	public void setImage(File image) {
+		this.image = image;
+	}
+
+	/**
+	 * @return the quantity
+	 */
+	public int getQuantity() {
+		return quantity;
+	}
+
+	/**
+	 * @param quantity the quantity to set
+	 */
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	/**
+	 * @return the price
+	 */
+	public int getPrice() {
+		return price;
+	}
+
+	/**
+	 * @param price the price to set
+	 */
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	/**
+	 * @return the classifications
+	 */
+	public HashMap<String, Byte> getClassifications() {
+		return classifications;
+	}
+
+	/**
+	 * Adds a new classification to the current wine
+	 * 
+	 * @param user  - the author of the classification
+	 * @param stars - the numerical value
+	 */
+	public void addClassification(User user, Byte stars) {
+		classifications.put(user.getName(), stars);
+	}
+
+}
