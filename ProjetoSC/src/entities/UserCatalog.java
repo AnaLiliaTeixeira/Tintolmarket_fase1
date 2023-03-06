bdbb8ab0b1777eccfcbf6ab92a60bfb79993e780
fc56277 <fc56277@alunos.fc.ul.pt>
2023-03-06 16:12:21 +0000

    Criadas classes que atuam como catalogo das informacoes do sistema

---
+++ ProjetoSC/src/entities/UserCatalog.java
@@ -0,0 +1,72 @@
+package entities;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.List;
+import java.util.Scanner;
+
+public class UserCatalog {
+
+	private List<User> users;
+	
+	// Nome para excecao mais especifico
+	public UserCatalog() throws IOException {
+		try {
+			File myObj = new File("userCreds.txt");
+			Scanner myReader = new Scanner(myObj);
+			while(myReader.hasNextLine()) {
+				String data = myReader.nextLine();
+				users.add(new User(data.split(":")[0]));
+				System.out.println();
+			}
+			myReader.close();
+		} catch (FileNotFoundException e) {
+			System.out.println("An error ocurred.");
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Faz login do utilizador ou cria um utilizador novo
+	 * 
+	 * @return o username se login com sucesso ou null, caso contrario
+	 * @throws Exception se ocorrer erro ao ler ou escrever
+	 */
+	public String login(ObjectInputStream in, ObjectOutputStream out) throws Exception {
+		File users = new File("userCreds.txt");
+		if (!users.exists())
+			users.createNewFile();
+		Scanner sc = new Scanner(users);
+
+		// le user e pass da socket
+		String user = (String) in.readObject();
+		String password = (String) in.readObject();
+
+		// verifica se user existe e pass esta correta
+		boolean newUser = true;
+		String line;
+		while (sc.hasNextLine()) {
+			if ((line = sc.nextLine()).startsWith(user) && line.endsWith(password)) { // se for a pass certa
+				newUser = false;
+				break;
+			} else if (line.startsWith(user)) { // se for a pass errada
+				sc.close();
+				return null;
+			}
+		}
+		sc.close();
+
+		// se o user nao existir faz o seu registo
+		if (newUser) {
+			FileWriter fw = new FileWriter("userCreds.txt");
+			fw.write(user + ":" + password);
+			fw.close();
+		}
+
+		return user;
+	}
+}
---
+++ ProjetoSC/src/entities/WineCatalog.java
@@ -0,0 +1,46 @@
+package entities;
+
+import java.io.File;
+import java.util.List;
+
+public class WineCatalog {
+
+	private List<Wine> wines;
+	
+	// Construtor WineCatalog (tirar duvidas com o stor primeiro)
+	
+	/**
+	 * @return the wines
+	 */
+	public List<Wine> getWines() {
+		return this.wines;
+	}
+	
+	public Wine getWine(String wineName) {
+		for (Wine w : this.wines)
+			if (w.getName().equals(wineName))
+				return w;
+		return null;
+	}
+
+	public boolean addWine(String wineName, File image) {
+		if(getWine(wineName) != null) {
+			return false;
+		}
+		this.wines.add(new Wine(wineName, image));
+		return true;
+	}
+	
+	//TODO
+	public boolean view(String wineName) {
+		StringBuilder sb = new StringBuilder();
+		Wine wine = this.getWine(wineName);
+		
+		sb.append("\t Informacoes sobre o vinho: \n");
+		sb.append("Imagem: " + wine.getImage());
+		//sb.append("Classificacao Media: " + wine);
+		// available wines to sell (neste caso é quando tem >0 vinhos na lista)sb.append(null)
+		// ter duas listas pode ser melhor
+		return false;
+	}
+}
