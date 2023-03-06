e7be89689ccf06659422c6f579a5f8c90f510164
fc56277 <fc56277@alunos.fc.ul.pt>
2023-03-06 16:11:10 +0000

    Handler para compras, vendas e criacao de anuncios

---
+++ ProjetoSC/src/application/TransactionHandler.java
@@ -0,0 +1,56 @@
+package application;
+
+import java.util.List;
+
+import entities.User;
+import entities.Wine;
+import entities.WineCatalog;
+
+public class TransactionHandler {
+
+	private WineCatalog wineCatalog;
+	
+	public TransactionHandler(WineCatalog wineCatalog) {
+		this.wineCatalog = wineCatalog;
+	}
+	
+	public boolean sell(User user, String name, double price, int quantity) {
+		
+		Wine w = wineCatalog.getWine(name);
+		if(w != null) {
+			user.createWineAd(w, quantity, price);
+			return true;
+		}
+		return false;
+	}
+	
+	public boolean buy(User buyer, String wineName, User seller, int quantity) {
+		double balance = buyer.getBalance();
+		Wine wine = wineCatalog.getWine(wineName);
+		if(wine == null) return false;
+		
+		WineAd wad=null;
+		int availableQuantity=0;
+		double priceToPay=0;
+		List<WineAd> wineAds = seller.getWineAds();
+		for(WineAd wa : wineAds) {
+			if(wa.getWine().equals(wine)) {
+				wad = wa;
+				availableQuantity = wa.getQuantity();
+				priceToPay = wa.getPrice()*quantity;
+				break;
+			}
+		}
+		
+		if(availableQuantity < quantity) return false;
+		if(priceToPay > balance) return false;
+		
+		buyer.adjustBalance(-priceToPay);
+		seller.adjustBalance(priceToPay);
+		
+		// add a lista do comprador (buyer) c/ a qnt que comprou do seller?? 
+		wad.adjustQuantity(-quantity);
+
+		return true;		
+	}
+}
