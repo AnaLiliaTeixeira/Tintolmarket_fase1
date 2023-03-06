f287c84cac021b43c6685076dc0a5de8911da29b
fc56277 <fc56277@alunos.fc.ul.pt>
2023-03-06 16:11:40 +0000

    Classe que representa um anuncio de um dado vinho por um utilizador

---
+++ ProjetoSC/src/application/WineAd.java
@@ -0,0 +1,32 @@
+package application;
+
+import entities.Wine;
+
+public class WineAd {
+
+	private Wine wine;
+	private int quantity;
+	private double price;
+	
+	public WineAd(Wine wine, int quantity, double price) {
+		this.wine = wine;
+		this.quantity = quantity;
+		this.price = price;
+	}
+	
+	public Wine getWine() {
+		return wine;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void adjustQuantity(int quantity) {
+		this.quantity+=quantity;
+	}
+	
+	public double getPrice() {
+		return price;
+	}
+}
