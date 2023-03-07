449b71473c378cbaf75c210c79493af3bdd950d0
AnaLiliaTeixeira <ana.lilia.teixeira2@gmail.com>
2023-03-07 17:49:39 +0000

    ObjectInputStream and ObjectOutputStream changed

--- ProjetoSC/src/application/MainClient.java
+++ ProjetoSC/src/application/MainClient.java
@@ -9,8 +9,8 @@ import java.util.Scanner;
 public class MainClient {
 
 	private static Socket socket;
-	private static ObjectInputStream in;
-	private static ObjectOutputStream out;
+//	private static ObjectInputStream in;
+//	private static ObjectOutputStream out;
 
 	public static void main(String[] args) {
 
@@ -25,15 +25,15 @@ public class MainClient {
 				socket = new Socket(serverInfo[0], 12345);
 
 			// iniciar streams
-			in = new ObjectInputStream(socket.getInputStream());
-			out = new ObjectOutputStream(socket.getOutputStream());
+			ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
+			ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
 
 			// enviar user e password
 			out.writeObject(args[1]);
 			out.writeObject(args[2]);
 
 			// interagir com o server
-			interact();
+			interact(in, out);
 
 			// fechar ligacoes
 			in.close();
@@ -46,30 +46,30 @@ public class MainClient {
 
 	}
 
-	private static void interact() throws Exception {
-		System.out.println("add <wine> <image> - adiciona um novo vinho identificado por wine, associado à imagem\r\n"
-				+ "image. Caso já exista um vinho com o mesmo nome deve ser devolvido um erro.\r\n"
-				+ "Inicialmente o vinho não terá qualquer classificação e o número de unidades disponíveis\r\n"
-				+ "será zero.\r\n"
-				+ "sell <wine> <value> <quantity> - coloca à venda o número indicado por quantity de\r\n"
-				+ "unidades do vinho wine pelo valor value. Caso o wine não exista, deve ser devolvido um\r\n"
-				+ "erro.\r\n" + "view <wine> - obtém as informações associadas ao vinho identificado por wine,\r\n"
-				+ "nomeadamente a imagem associada, a classificação média e, caso existam unidades do\r\n"
-				+ "vinho disponíveis para venda, a indicação do utilizador que as disponibiliza, o preço e a\r\n"
-				+ "quantidade disponível. Caso o vinho wine não exista, deve ser devolvido um erro.\r\n"
+	private static void interact(ObjectInputStream in, ObjectOutputStream out) throws Exception {
+		System.out.println("add <wine> <image> - adiciona um novo vinho identificado por wine, associado a imagem\r\n"
+				+ "image. Caso ja exista um vinho com o mesmo nome deve ser devolvido um erro.\r\n"
+				+ "Inicialmente o vinho nao tera qualquer classificacao e o numero de unidades disponiveis\r\n"
+				+ "sera zero.\r\n"
+				+ "sell <wine> <value> <quantity> - coloca a venda o numero indicado por quantity de\r\n"
+				+ "unidades do vinho wine pelo valor value. Caso o wine nao exista, deve ser devolvido um\r\n"
+				+ "erro.\r\n" + "view <wine> - obtem as informacoes associadas ao vinho identificado por wine,\r\n"
+				+ "nomeadamente a imagem associada, a classificacao media e, caso existam unidades do\r\n"
+				+ "vinho disponiveis para venda, a indicacao do utilizador que as disponibiliza, o preco e a\r\n"
+				+ "quantidade disponivel. Caso o vinho wine nao exista, deve ser devolvido um erro.\r\n"
 				+ "buy <wine> <seller> <quantity> - compra quantity unidades do vinho wine ao utilizador\r\n"
-				+ "seller. O número de unidades deve ser removido da quantidade disponível e deve ser\r\n"
-				+ "transferido o valor correspondente à compra da conta do comprador para o vendedor.\r\n"
-				+ "Caso o vinho não exista, ou não existam unidades suficientes, ou o comprador não tenha\r\n"
-				+ "saldo suficiente, deverá ser devolvido e assinalado o erro correspondente.\r\n"
-				+ "wallet - obtém o saldo atual da carteira.\r\n"
-				+ "classify <wine> <stars> - atribui ao vinho wine uma classificação de 1 a 5, indicada por stars.\r\n"
-				+ "Caso o vinho wine não exista, deve ser devolvido um erro.\r\n"
+				+ "seller. O numero de unidades deve ser removido da quantidade disponivel e deve ser\r\n"
+				+ "transferido o valor correspondente a compra da conta do comprador para o vendedor.\r\n"
+				+ "Caso o vinho nao exista, ou nao existam unidades suficientes, ou o comprador nao tenha\r\n"
+				+ "saldo suficiente, devera ser devolvido e assinalado o erro correspondente.\r\n"
+				+ "wallet - obtem o saldo atual da carteira.\r\n"
+				+ "classify <wine> <stars> - atribui ao vinho wine uma classificacao de 1 a 5, indicada por stars.\r\n"
+				+ "Caso o vinho wine nao exista, deve ser devolvido um erro.\r\n"
 				+ "talk <user> <message> - permite enviar uma mensagem privada ao utilizador user (por\r\n"
-				+ "exemplo, uma pergunta relativa a um vinho à venda). Caso o utilizador não exista, deve\r\n"
+				+ "exemplo, uma pergunta relativa a um vinho a venda). Caso o utilizador nao exista, deve\r\n"
 				+ "ser devolvido um erro.\r\n" + "3\r\n"
-				+ "read - permite ler as novas mensagens recebidas. Deve ser apresentada a identificação do\r\n"
-				+ "remetente e a respetiva mensagem. As mensagens são removidas da caixa de mensagens\r\n"
+				+ "read - permite ler as novas mensagens recebidas. Deve ser apresentada a identificacao do\r\n"
+				+ "remetente e a respetiva mensagem. As mensagens sao removidas da caixa de mensagens\r\n"
 				+ "do servidor depois de serem lidas.");
 
 		Scanner sc = new Scanner(System.in);
