fae6a502731ae1f3b8366de5c961aba4c43d35cf
fc56277 <fc56277@alunos.fc.ul.pt>
2023-03-07 12:30:58 +0000

    Add metodos: - toString (usado em View) - equals

--- ProjetoSC/src/entities/WineAd.java
+++ ProjetoSC/src/entities/WineAd.java
@@ -27,4 +27,15 @@ public class WineAd {
 	public double getPrice() {
 		return price;
 	}
+	
+	//TODO
+	public String toString;
+	
+	//TODO
+	// Remover vinho da lista de anuncios (pode nao ser preciso)
+	public boolean equals(Object o) {
+		return false;
+		//return this == 0 || o instanceof WineAd && ;
+	}
+	
 }
