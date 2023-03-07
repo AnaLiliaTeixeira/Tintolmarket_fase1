6b717471b4311d2c8f9170dd889440bcf601493f
joaorsmatos02 <joao-matos02@hotmail.com>
2023-03-07 19:07:33 +0000

    Added singleton pattern

--- ProjetoSC/src/catalogs/WineCatalog.java
+++ ProjetoSC/src/catalogs/WineCatalog.java
@@ -1,23 +1,45 @@
 package catalogs;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 
 import entities.Wine;
 
 public class WineCatalog {
+	
+	/*
+	 * um ficheiro com user:password de todos os users
+	 * um ficheiro por user com saldo, anuncios etc.
+	 * um ficheiro com todos os vinhos
+	 * 
+	 */
 
 	private List<Wine> wines;
-	
+	private static WineCatalog instance;
+
+	private WineCatalog() {
+		this.wines = new ArrayList<>();
+		// chamar metodos
+	}
+
+	public static WineCatalog getInstance() {
+		if (instance == null) {
+			instance = new WineCatalog();
+			// inicializar
+		}
+		return instance;
+	}
+
 	// Construtor WineCatalog (tirar duvidas com o stor primeiro)
-	
+
 	/**
 	 * @return the wines
 	 */
 	public List<Wine> getWines() {
 		return this.wines;
 	}
-	
+
 	public Wine getWine(String wineName) {
 		for (Wine w : this.wines)
 			if (w.getName().equals(wineName))
@@ -26,13 +48,13 @@ public class WineCatalog {
 	}
 
 	public boolean addWine(String wineName, File image) {
-		if(getWine(wineName) != null) {
+		if (getWine(wineName) != null) {
 			return false;
 		}
 		this.wines.add(new Wine(wineName, image));
 		return true;
 	}
-	
+
 	public boolean view(String wineName) {
 		Wine wine = this.getWine(wineName);
 		if (wine == null) {
