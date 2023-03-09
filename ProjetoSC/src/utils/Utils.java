7a658cee6b888b59f1a396e8307589f8ce98c4b9
joaorsmatos02 <joao-matos02@hotmail.com>
2023-03-09 17:47:06 +0000

    Added replaceLine method

--- ProjetoSC/src/utils/Utils.java
+++ ProjetoSC/src/utils/Utils.java
@@ -1,11 +1,30 @@
 package utils;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.util.Scanner;
 
 public class Utils {
 
 	public static void replaceLine(File file, String oldLine, String newLine) {
-
+		try {
+			File newFile = new File("storedFiles//temp.txt");
+			FileWriter fw = new FileWriter(newFile, true);
+			newFile.createNewFile();
+			Scanner sc = new Scanner(file);
+			while (sc.hasNextLine()) {
+				String next = sc.nextLine();
+				if (next.equals(oldLine))
+					fw.append(newLine);
+				else
+					fw.append(next);
+			}
+			newFile.renameTo(file);
+			fw.close();
+			sc.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
