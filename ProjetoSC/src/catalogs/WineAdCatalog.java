9be624adee25c616360498ba1da3977542703568
joaorsmatos02 <joao-matos02@hotmail.com>
2023-03-09 15:49:55 +0000

    Grammar

--- ProjetoSC/src/catalogs/WineAdCatalog.java
+++ ProjetoSC/src/catalogs/WineAdCatalog.java
@@ -15,84 +15,91 @@ import entities.WineAd;
 public class WineAdCatalog {
 
 	// Private static instance variable of the class
-    private static WineAdCatalog instance;
+	private static WineAdCatalog instance;
 	private List<WineAd> wineAds;
-	
+
 	private WineAdCatalog() {
 		wineAds = new ArrayList<>();
-		getWineAdsByTextFile();
+		File wineAdsInfo = new File("storedFiles\\wineAdsCatalog.txt");
+		try {
+			if (!wineAdsInfo.exists())
+				wineAdsInfo.createNewFile();
+			else
+				getWineAdsByTextFile(wineAdsInfo);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
-	
-    public static WineAdCatalog getInstance() {
-        if (instance == null) {
-            instance = new WineAdCatalog();
-        }
-        return instance;
-    }
-    
-    private void getWineAdsByTextFile() {
-    	File wineInfo = new File("storedFiles\\wineAdsCatalog.txt");
+
+	public static WineAdCatalog getInstance() {
+		if (instance == null) {
+			instance = new WineAdCatalog();
+		}
+		return instance;
+	}
+
+	private void getWineAdsByTextFile(File wineAdsInfo) {
 		Scanner sc = null;
 		try {
-			sc = new Scanner(wineInfo);
+			sc = new Scanner(wineAdsInfo);
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
-		
-		while(sc.hasNextLine()) {
+
+		while (sc.hasNextLine()) {
 			String[] line = sc.nextLine().split(" ");
 			UserCatalog uc = UserCatalog.getInstance();
 			WineCatalog wc = WineCatalog.getInstance();
-			wineAds.add(new WineAd(uc.getUserByName(line[0]), wc.getWineByName(line[1]),
-						Integer.parseInt(line[2]), Double.parseDouble(line[3])));
+			wineAds.add(new WineAd(uc.getUserByName(line[0]), wc.getWineByName(line[1]), Integer.parseInt(line[2]),
+					Double.parseDouble(line[3])));
 			break;
 		}
 		sc.close();
-    }
-    
-    public List<WineAd> getWineAdsByUser(User user) {
-    	List<WineAd> list = new ArrayList<>();
-    	for (WineAd ad : wineAds) {
-    		if (ad.getUser().equals(user)) {
-    			list.add(ad);
-    		}
-    	}
-    	return list;
-    }
-    
-    public List<WineAd> getWineAdsByWine(Wine wine) {
-    	List<WineAd> list = new ArrayList<>();
-    	for (WineAd ad : wineAds) {
-    		if (ad.getWine().equals(wine)) {
-    			list.add(ad);
-    		}
-    	}
-    	return list;
-    }
-    
-    public void add(WineAd wineAd) {
-    	try {
-			File wineAdInfo = new File ("storedFiles\\wineAdsCatalog.txt");
+	}
+
+	public List<WineAd> getWineAdsByUser(User user) {
+		List<WineAd> list = new ArrayList<>();
+		for (WineAd ad : wineAds) {
+			if (ad.getUser().equals(user)) {
+				list.add(ad);
+			}
+		}
+		return list;
+	}
+
+	public List<WineAd> getWineAdsByWine(Wine wine) {
+		List<WineAd> list = new ArrayList<>();
+		for (WineAd ad : wineAds) {
+			if (ad.getWine().equals(wine)) {
+				list.add(ad);
+			}
+		}
+		return list;
+	}
+
+	public void add(WineAd wineAd) {
+		try {
+			File wineAdInfo = new File("storedFiles\\wineAdsCatalog.txt");
 			FileWriter fw = new FileWriter(wineAdInfo, true);
 			fw.write(this.toString() + "\r\n");
-	    	this.wineAds.add(wineAd);
+			this.wineAds.add(wineAd);
 			fw.close();
-		} catch (IOException e){
-			e.printStackTrace();		
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
-    }
-    
-    // TODO 
-    public void remove(WineAd wineAd) {
-    	try {
-			File wineAdInfo = new File ("storedFiles\\wineAdsCatalog.txt");
+	}
+
+	// TODO
+	public void remove(WineAd wineAd) {
+		try {
+			File wineAdInfo = new File("storedFiles\\wineAdsCatalog.txt");
 			FileWriter fw = new FileWriter(wineAdInfo, true);
 			fw.write(this.toString() + "\r\n");
 			fw.close();
-		} catch (IOException e){
-			e.printStackTrace();		
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
-    	
-    	this.wineAds.remove(wineAd);
-    }
+
+		this.wineAds.remove(wineAd);
+	}
 }
