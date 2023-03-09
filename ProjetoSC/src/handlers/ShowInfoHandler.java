32220b866522fcaf84fd4e20d0607a17f7c77e3f
joaorsmatos02 <joao-matos02@hotmail.com>
2023-03-09 17:46:20 +0000

    Changed view method

--- ProjetoSC/src/handlers/ShowInfoHandler.java
+++ ProjetoSC/src/handlers/ShowInfoHandler.java
@@ -6,24 +6,17 @@ import entities.Wine;
 
 public class ShowInfoHandler {
 
-	// view
-	public static boolean view(String wineName) {
+	public static String view(String wineName) {
 		Wine wine = WineCatalog.getInstance().getWineByName(wineName);
-		if (wine == null) {
-			return false;
-		}
-		System.out.println("Informacoes sobre o vinho: \n");
-		System.out.println(wine.printWine());
-
-		return true;
+		if (wine == null)
+			return null;
+		return wine.printWine();
 	}
 
-	// wallet
 	public static String wallet(User user) {
 		return "O saldo utilizador " + user.getName() + " e " + user.getBalance();
 	}
 
-	// read
 	public static String read(User user) {
 		String inbox = user.inboxToString();
 		user.deleteMessages();
