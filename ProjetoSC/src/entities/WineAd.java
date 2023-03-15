f5b1cee0b8d3d84a12c2afde9a1c6609455a3700
fc56277 <fc56277@alunos.fc.ul.pt>
2023-03-15 22:58:43 +0000

    Javadoc class WineAd feito

--- ProjetoSC/src/entities/WineAd.java
+++ ProjetoSC/src/entities/WineAd.java
@@ -5,6 +5,10 @@ import java.util.Objects;
 
 import utils.Utils;
 
+/**
+
+A classe WineAd representa um anúncio de vinho no sistema. Cada anúncio contém informações sobre o utilizador que o criou, o vinho anunciado, o preço e a quantidade disponível.
+*/
 public class WineAd {
 
 	private User user;
@@ -12,6 +16,14 @@ public class WineAd {
 	private double price;
 	private int quantity;
 
+/**
+
+	Construtor da classe WineAd.
+	@param user O utilizador que criou o anúncio.
+	@param wine O vinho anunciado.
+	@param price O preço do vinho.
+	@param quantity A quantidade disponível.
+*/
 	public WineAd(User user, Wine wine, double price, int quantity) {
 		this.user = user;
 		this.wine = wine;
@@ -19,18 +31,39 @@ public class WineAd {
 		this.quantity = quantity;
 	}
 
+/**
+
+	Obtém o utilizador que criou o anúncio.
+	@return O utilizador que criou o anúncio.
+*/
 	public User getUser() {
 		return user;
 	}
 
+/**
+
+	Obtém o vinho anunciado.
+	@return O vinho anunciado.
+*/
 	public Wine getWine() {
 		return wine;
 	}
 
+/**
+
+	Obtém a quantidade disponível do vinho no anúncio.
+	@return A quantidade disponível.
+*/
 	public int getQuantity() {
 		return quantity;
 	}
 
+/**
+
+	Ajusta a quantidade e o preço do anúncio.
+	@param quantity A nova quantidade.
+	@param price O novo preço.
+*/
 	public void adjustQuantityAndPrice(int quantity, double price) {
 		String oldLine = this.toString();
 		this.quantity += quantity;
@@ -39,10 +72,24 @@ public class WineAd {
 		Utils.replaceLine(new File("wineAdsCatalog.txt"), oldLine, newLine);
 	}
 
+/**
+
+	Obtem o preco do vinho no anuncio.
+	@return O preco do vinho.
+*/
 	public double getPrice() {
 		return price;
 	}
 
+/**
+
+	Ajusta a quantidade do anuncio.
+	@param i A nova quantidade.
+*/
+	public void setQuantity(int i) {
+		this.quantity=i;
+	}
+	
 	@Override
 	public String toString() {
 		return user.getName() + " " + this.wine.getName() + " " + this.price + " " + this.quantity;
@@ -59,9 +106,4 @@ public class WineAd {
 		WineAd other = (WineAd) obj;
 		return Objects.equals(user, other.user) && Objects.equals(wine, other.wine);
 	}
-
-	public void setQuantity(int i) {
-		this.quantity=i;
-	}
-
 }
