42852f2207ef61e3df136d67ffed70b132006a8a
fc56277 <fc56277@alunos.fc.ul.pt>
2023-03-15 22:51:16 +0000

    Javadoc class Wine feito

--- ProjetoSC/src/entities/Wine.java
+++ ProjetoSC/src/entities/Wine.java
@@ -8,64 +8,92 @@ import java.util.Objects;
 import catalogs.WineAdCatalog;
 import utils.Utils;
 
+/**
+
+	A classe Wine representa um vinho no sistema. Cada vinho tem um nome, imagem e classificações.
+
+	Os vinhos podem ter anúncios associados e classificações adicionadas por utilizadores.
+*/
 public class Wine {
 
 	private String name;
 	private File image;
 	private HashMap<String, Integer> classifications;
 
+/**
+
+	Construtor da classe Wine.
+	@param name O nome do vinho.
+	@param image A imagem do vinho.
+	@param classifications As classificações do vinho.
+*/
 	public Wine(String name, File image, HashMap<String, Integer> classifications) {
 		this.name = name;
 		this.image = image;
 		this.classifications = classifications;
 	}
 
-	/**
-	 * @return the name
-	 */
+/**
+
+	Obtém o nome do vinho.
+	@return O nome do vinho.
+*/
 	public String getName() {
 		return name;
 	}
 
-	/**
-	 * @param name the name to set
-	 */
+/**
+
+	Define o nome do vinho.
+	@param name O nome a ser definido.
+*/
 	public void setName(String name) {
 		this.name = name;
 	}
 
-	/**
-	 * @return the image
-	 */
+/**
+
+	Obtém a imagem do vinho.
+	@return A imagem do vinho.
+*/
 	public File getImage() {
 		return image;
 	}
 
-	/**
-	 * @param image the image to set
-	 */
+/**
+
+	Define a imagem do vinho.
+	@param image A imagem a ser definida.
+*/
 	public void setImage(File image) {
 		this.image = image;
 	}
 
+/**
+
+	Obtém os anúncios atuais do vinho.
+	@return A lista de anúncios atuais do vinho.
+*/
 	public List<WineAd> getCurrentAds() {
 		WineAdCatalog wineAds = WineAdCatalog.getInstance();
 		return wineAds.getWineAdsByWine(this);
 	}
 
-	/**
-	 * @return the classifications
-	 */
+/**
+
+	Obtém as classificações do vinho.
+	@return As classificações do vinho.
+*/
 	public HashMap<String, Integer> getClassifications() {
 		return classifications;
 	}
 
-	/**
-	 * Adds a new classification to the current wine
-	 * 
-	 * @param user  - the author of the classification
-	 * @param stars - the numerical value
-	 */
+/**
+
+	Adiciona uma nova classificação ao vinho.
+	@param user O autor da classificação.
+	@param stars O valor numérico da classificação.
+*/
 	public void addClassification(User user, Integer stars) {
 		String oldLine = this.toString();
 		this.classifications.put(user.getName(), stars);
@@ -74,6 +102,12 @@ public class Wine {
 		Utils.replaceLine(wineInfo, oldLine, newLine);
 	}
 
+/**
+
+	Retorna uma representação textual do vinho, incluindo média das classificações e informações de venda.
+
+	@return A representação em texto do vinho.
+*/
 	public String printWine() {
 		StringBuilder sb = new StringBuilder();
 		sb.append("Nome do Vinho: " + name + " - " + image.getName() + "\n");
