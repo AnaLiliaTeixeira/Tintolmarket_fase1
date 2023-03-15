df6e5c781cc6a8d2202a2ff2d86731a80051c167
fc56277 <fc56277@alunos.fc.ul.pt>
2023-03-15 23:12:57 +0000

    Javadoc class AddInfoHandler feito

--- ProjetoSC/src/handlers/AddInfoHandler.java
+++ ProjetoSC/src/handlers/AddInfoHandler.java
@@ -11,12 +11,31 @@ import exceptions.RepeatedWineException;
 import exceptions.UserNotFoundException;
 import exceptions.WineNotFoundException;
 
+/**
+
+	A classe AddInfoHandler é responsável por lidar com as operações de adição e classificação de vinhos e troca de mensagens entre usuários.
+*/
 public class AddInfoHandler {
 
+/**
+
+	Adiciona um novo vinho ao catálogo de vinhos e associa uma imagem a ele.
+	@param wineName O nome do novo vinho.
+	@param image O arquivo de imagem do novo vinho.
+	@throws RepeatedWineException Se já existir um vinho com o mesmo nome.
+*/
 	public static void add(String wineName, File image) throws RepeatedWineException {
 		WineCatalog.getInstance().createWine(wineName, image);
 	}
 
+/**
+
+	Classifica um vinho com base na pontuação fornecida por um usuário.
+	@param user O usuário que classifica o vinho.
+	@param wine O nome do vinho a ser classificado.
+	@param stars A pontuação atribuída ao vinho (1 a 5 estrelas).
+	@throws WineNotFoundException Se o vinho não for encontrado no catálogo.
+*/
 	public static void classify(User user, String wine, int stars) throws WineNotFoundException {
 		Wine w = WineCatalog.getInstance().getWineByName(wine);
 		if (stars < 1 || stars > 5)
@@ -26,6 +45,15 @@ public class AddInfoHandler {
 		w.addClassification(user, stars);
 	}
 
+/**
+
+	Adiciona uma mensagem enviada por um usuário a outro usuário.
+	@param sender O usuário que envia a mensagem.
+	@param recipient O nome do usuário destinatário da mensagem.
+	@param message O conteúdo da mensagem.
+	@throws IOException Se ocorrer um erro de entrada/saída.
+	@throws UserNotFoundException Se o usuário destinatário não for encontrado.
+*/
 	public static void talk(User sender, String recipient, String message) throws IOException, UserNotFoundException {
 		User destinatary = UserCatalog.getInstance().getUserByName(recipient);
 		if (destinatary == null)
