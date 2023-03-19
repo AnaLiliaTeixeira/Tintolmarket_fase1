5ef2603eea1b100d856855f9339c8ca066e511b3
AnaLiliaTeixeira <ana.lilia.teixeira2@gmail.com>
2023-03-19 15:22:27 +0000

    Tratamento de args invalidos no cliente

--- ProjetoSC/src/application/Tintolmarket.java
+++ ProjetoSC/src/application/Tintolmarket.java
@@ -22,6 +22,11 @@ public class Tintolmarket {
 
 	public static void main(String[] args) {
 
+		if (args.length != 3) {
+			System.out.println("A aplicação deve ser iniciada da forma Tintolmarket <serverAddress> <userID> [password]");
+			System.exit(0);
+		}
+		
 		// retirar ip e port
 		String[] serverInfo = args[0].split(":");
 
@@ -65,7 +70,7 @@ public class Tintolmarket {
 	 *
 	 * @param in  ObjectInputStream para ler dados do servidor.
 	 * @param out ObjectOutputStream para enviar dados para o servidor.
-	 * @throws Exception Se ocorrer algum erro durante a intera�ao com o servidor.
+	 * @throws Exception Se ocorrer algum erro durante a intera�ao com o servidor.
 	 */
 	private static void interact(DataInputStream in, DataOutputStream out) throws Exception {
 		System.out.println(
@@ -90,6 +95,7 @@ public class Tintolmarket {
 			String line = sc.nextLine();
 			String[] tokens = line.split(" ");
 			boolean image = false;
+			boolean wait = true;
 			if (tokens[0].equals("a") || tokens[0].equals("add")) {
 				if (tokens.length != 3)
 					System.out.println("O comando add e usado na forma \"add <wine> <image>\"");
@@ -107,11 +113,14 @@ public class Tintolmarket {
 						file.close();
 					} else {
 						System.out.println("A imagem " + tokens[2] + " nao existe!");
+						wait = false;
 					}
 				}
 			} else if (tokens[0].equals("s") || tokens[0].equals("sell")) {
-				if (tokens.length != 4)
+				if (tokens.length != 4) {
 					System.out.println("O comando sell e usado na forma \"sell <wine> <value> <quantity>\"");
+					wait = false;
+				}
 				else {
 					out.writeUTF("s");
 					out.writeUTF(tokens[1]);
@@ -119,39 +128,49 @@ public class Tintolmarket {
 					out.writeUTF(tokens[3]);
 				}
 			} else if (tokens[0].equals("v") || tokens[0].equals("view")) {
-				if (tokens.length != 2)
+				if (tokens.length != 2) {
 					System.out.println("O comando view e usado na forma \"view <wine>\"");
+					wait = false;
+				}
 				else {
 					out.writeUTF("v");
 					out.writeUTF(tokens[1]);
 					image = true;
 				}
 			} else if (tokens[0].equals("b") || tokens[0].equals("buy")) {
-				if (tokens.length != 4)
+				if (tokens.length != 4) {
 					System.out.println("O comando buy e usado na forma \"buy <wine> <seller> <quantity>\"");
-				else {
+					wait = false;
+
+				} else {
 					out.writeUTF("b");
 					out.writeUTF(tokens[1]);
 					out.writeUTF(tokens[2]);
 					out.writeUTF(tokens[3]);
 				}
 			} else if (tokens[0].equals("w") || tokens[0].equals("wallet")) {
-				if (tokens.length != 1)
+				if (tokens.length != 1) {
 					System.out.println("O comando wallet e usado na forma \"wallet\"");
+					wait = false;
+				}
 				else {
 					out.writeUTF("w");
 				}
 			} else if (tokens[0].equals("c") || tokens[0].equals("classify")) {
-				if (tokens.length != 3)
+				if (tokens.length != 3) {
 					System.out.println("O comando classify e usado na forma \"classify <wine> <stars>\"");
+					wait = false;
+				}
 				else {
 					out.writeUTF("c");
 					out.writeUTF(tokens[1]);
 					out.writeUTF(tokens[2]);
 				}
 			} else if (tokens[0].equals("t") || tokens[0].equals("talk")) {
-				if (tokens.length < 3)
+				if (tokens.length < 3) { 
 					System.out.println("O comando talk e usado na forma \"talk <user> <message>\"");
+					wait = false;
+				}
 				else {
 					StringBuilder sb = new StringBuilder();
 					for (int i = 2; i < tokens.length; i++) {
@@ -162,15 +181,18 @@ public class Tintolmarket {
 					out.writeUTF(sb.toString());
 				}
 			} else if (tokens[0].equals("r") || tokens[0].equals("read")) {
-				if (tokens.length != 1)
+				if (tokens.length != 1) {
 					System.out.println("O comando read e usado na forma \"read \"");
+					wait = false;
+				}
 				else {
 					out.writeUTF("r");
 				}
 			} else if (tokens[0].equals("exit")) {
-				System.out.println("A encerrar programa...");
+				System.out.println("Programa encerrado.");
 				out.writeUTF("exit");
 				exit = true;
+				wait = false;
 			} else {
 				System.out.println("Comando nao reconhecido");
 				System.out.println(
@@ -189,7 +211,7 @@ public class Tintolmarket {
 								+ "\tread - permite ler as novas mensagens recebidas.");
 			}
 
-			if (!exit) {
+			if (wait) {
 				System.out.println(in.readUTF());
 			}
 			if (image) {
